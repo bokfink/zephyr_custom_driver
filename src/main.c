705aee7979f44ef6c6798f902ff011fe55d6c691
Pullupresistor <i@pullupresistor.com>
2023-06-07 17:23:01 +0900

    First build with nothing

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+
+enum rotation_direction {
+  CLOCKWISE,
+  COUNTER_CLOCKWISE,
+};
+
+int take_steps(const uint32_t target_num_steps,
+	       const enum rotation_direction rot_dir,
+	       const int32_t sleep_time_ms)
+{
+  return 0;
+}
+	       
